16ed1b1844efe967ee70f1119fdaee0a6ddd9fb9
Elliott Murray <emurray@dius.com.au>
2016-04-03 14:30:48 +1000

    changed to mocha style

--- test/service_consumers/pact_helper.js
+++ test/service_consumers/pact_helper.js
@@ -2,10 +2,8 @@
 import q from 'q';
 
 import request from 'request'
-import Pact from 'pact-js-provider'
-let pact = new Pact()
 
-// pact.serviceProvider("fooProvider", () => {
+// serviceProvider("fooProvider", () => {
 //   honoursPactWith('fooConsumer', () => {
 //     pactUri('pacts/fooconsumer-fooprovider.json')
 //   })
@@ -21,49 +19,44 @@ function switchMary(turnOn, done) {
     }
   }
 
-  request(post_options, (error, req, resp) =>{
+  request(post_options, (error, req, resp) => {
     console.log('callback on request')
     done()
   })
-  }
+}
 
-pact.provider_states_for('Zoo App', () => {
-
-  pact.providerState('there is an alligator named Mary', {
-    file: './pacts/zoo_app-animal_service.json',
-    setup: (done) => {
+provider_states_for('Animal Service', 'Zoo App', './pacts/zoo_app-animal_service.json', 'http://localhost:5000', (done) => {
+  providerState('there is an alligator named Mary', (done) => {
+    setUp( (done) => {
       console.log('setup111111')
-      return switchMary(false, done)
-    },
+      switchMary(true, done)
+    })
 
-    options: {"pactUrl":'./pacts/zoo_app-animal_service.json',
-        "baseUrl": 'http://localhost:5000'},
-
-    teardown: (done) => {
-      console.log('teardown')
+    it( (done) => {
       done()
-    }
+    })
 
-  }).then( () => {
-
-    pact.providerState('there is not an alligator named Mary', {
-      file: './pacts/zoo_app-animal_service.json',
-      options: {"pactUrl":'./pacts/zoo_app-animal_service.json',
-          "baseUrl": 'http://localhost:5000'},
-
-      setup: (done) => {
-        console.log('setup22222')
-        return switchMary(false, done)
-        // console.log('setup22222')
-      },
-
-      teardown: (done) => {
-        console.log('teardown')
-        done()
-        // return
-      }
+    tearDown( (done) => {
+      done()
     })
   })
 
-}).verify()
+  providerState('there is not an alligator named Mary', (done) => {
+    let file = './pacts/zoo_app-animal_service.json'
+    let options = {"pactUrl":'./pacts/zoo_app-animal_service.json', "baseUrl": 'http://localhost:5000'}
 
+    setUp((done) => {
+      console.log('setup22222')
+      switchMary(false, done)
+    })
+
+    it( (done) => {
+      done()
+    })
+
+    tearDown( (done) => {
+      done()
+    })
+
+  })
+})
